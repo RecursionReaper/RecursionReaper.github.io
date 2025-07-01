047df59843452329f5cd44e960e8e9aa6a674a47
RecursionReaper <57366612+RecursionReaper@users.noreply.github.com>
2025-07-01 21:14:10 +0000

    Deploying to gh-pages from @ RecursionReaper/RecursionReaper.github.io@ca658c7c5b06953d945d6e9ec56dd4bd8a1eda72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6E%69%6B%65%74%64%65%73%61%69%32%30%30%35@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
