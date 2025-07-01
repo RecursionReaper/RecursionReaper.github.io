f1c9d3532580ba445cb92e2f53d6fa1d2b56e65d
RecursionReaper <57366612+RecursionReaper@users.noreply.github.com>
2025-07-01 23:04:14 +0000

    Deploying to gh-pages from @ RecursionReaper/RecursionReaper.github.io@8894d21c7667499dfd5f8aca875c80983d358eae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,8 +428,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
+      },{id: "books-a-master-39-s-secret-whispers",
+          title: 'A Master&amp;#39;s Secret Whispers',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
